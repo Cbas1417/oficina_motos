5aa16ae46774546306bd32425f8fc58c8705b0b2
JoseM <josem160n@gmail.com>
2025-09-10 15:33:14 -0500

    apartado correos

--- FRONT-END/usuario/clientes.js
+++ FRONT-END/usuario/clientes.js
@@ -1,9 +1,9 @@
 const tbody = document.getElementById("clientes-cuerpo");
 
 window.clientes = [  
-    { id: 1, nombre: "Carlos Pérez", registro: "2025-08-10", fin: "2025-09-10", tipov: "Moto", cobro:192000, celular:"3005670124", empleado:"Sebas", lugar:"Caldas" },
-    { id: 2, nombre: "Laura Gómez", registro: "2025-07-05", fin: "2025-08-05", tipov: "Carro", cobro:195000, celular:"3007270124", empleado:"Isabela", lugar:"Caldas" },
-    { id: 3, nombre: "Pedro Ruiz", registro: "2025-06-20", fin: "2025-07-20", tipov: "Moto", cobro:200000, celular:"3005656124", empleado:"Daniel", lugar:"Envigado" }
+    { id: 1, nombre: "Carlos Pérez", registro: "2025-08-10", fin: "2025-09-10", tipov: "Moto", cobro:192000, celular:"3005670124",correo:"carlos@gmail.com", empleado:"Sebas", lugar:"Caldas" },
+    { id: 2, nombre: "Laura Gómez", registro: "2025-07-05", fin: "2025-08-05", tipov: "Carro", cobro:195000, celular:"3007270124",correo:"Laura@gmail.com", empleado:"Isabela", lugar:"Caldas" },
+    { id: 3, nombre: "Pedro Ruiz", registro: "2025-06-20", fin: "2025-07-20", tipov: "Moto", cobro:200000, celular:"3005656124",correo:"Pedro@gmail.com", empleado:"Daniel", lugar:"Envigado" }
 ];
 
 window.renderTabla = function renderTabla() {
@@ -20,6 +20,7 @@ window.renderTabla = function renderTabla() {
             <td><input type="text" value="${cliente.tipov}" disabled></td>
             <td><input type="text" value="${new Intl.NumberFormat('es-CO', { style: 'currency', currency: 'COP' }).format(cliente.cobro)}" disabled></td>
             <td><input type="text" value="${cliente.celular}" disabled></td>
+            <td><input type="email" value="${cliente.correo}" disabled></td>
             <td><input type="text" value="${cliente.empleado}" disabled></td>
             <td><input type="text" value="${cliente.lugar}" disabled></td>
             <td>
